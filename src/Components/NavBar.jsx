d93d1febece636145bfca07fb6cb79647a2dfdae
JuPiazzoli <jujubydomeneghetti@gmail.com>
2025-09-30 19:35:50 -0300

    Modificação - NavBar (Deixar mais coerente para celular)

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,33 +1,47 @@
+
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import "./NavBar.css";
 
 function Navbar() {
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
   return (
     <nav className="navbar">
-      <ul className="nav-links">
+      {/* Ícone do menu hamburguer */}
+      <div className="menu-icon" onClick={toggleMenu}>
+        ☰
+      </div>
+
+      {/* Links só aparecem se menuOpen for true */}
+      <ul className={`nav-links ${menuOpen ? "active" : ""}`}>
         <li>
-          <Link to="/">Apresentação</Link>
+          <Link to="/" onClick={toggleMenu}>Apresentação</Link>
         </li>
         <li>
-          <Link to="/Capacitismo">Capacitismo</Link>
+          <Link to="/Capacitismo" onClick={toggleMenu}>Capacitismo</Link>
         </li>
         <li>
-          <Link to="/Autismo">Autismo</Link>
+          <Link to="/Autismo" onClick={toggleMenu}>Autismo</Link>
         </li>
         <li>
-          <Link to="/Tdah">TDAH</Link>
+          <Link to="/Tdah" onClick={toggleMenu}>TDAH</Link>
         </li>
         <li>
-          <Link to="/Deficiencia-Auditiva">Def. Auditiva</Link>
+          <Link to="/Deficiencia-Auditiva" onClick={toggleMenu}>Def. Auditiva</Link>
         </li>
         <li>
-          <Link to="/Deficiencia-Fisica">Def. Física</Link>
+          <Link to="/Deficiencia-Fisica" onClick={toggleMenu}>Def. Física</Link>
         </li>
         <li>
-          <Link to="/Deficiencia-Intelectual">Def. Intelectual</Link>
+          <Link to="/Deficiencia-Intelectual" onClick={toggleMenu}>Def. Intelectual</Link>
         </li>
         <li>
-          <Link to="/Deficiencia-Visual">Def. Visual</Link>
+          <Link to="/Deficiencia-Visual" onClick={toggleMenu}>Def. Visual</Link>
         </li>
       </ul>
     </nav>
